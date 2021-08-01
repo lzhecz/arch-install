784e6534aeb91e4b75c68fc707b7fcf7282b64e8
lzhecz <34045305+lzhecz@users.noreply.github.com>
2021-08-01 22:11:23 +0300

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,25 @@
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c Ukraine -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
+
+sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+sudo firewall-cmd --add-port=1025-65535/udp --permanent
+sudo firewall-cmd --reload
+
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur/
+makepkg -si --noconfirm
+
+#sudo pacman -S --noconfirm xorg
+sudo pacman -S --noconfirm gdm gnome firefox gnome-tweaks simplescreenrecorder obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+sudo pikaur -S --noconfirm telegram-desktop-bin
+sudo pikaur -S --noconfirm discord
+sudo pikaur -S --noconfirm stacer
+
+sudo systemctl enable gdm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+sudo reboot
