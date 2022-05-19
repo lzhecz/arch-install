8d087583d555350bdfade70ade06755086477a9f
lzhecz <vladxpok@gmail.com>
2022-05-19 19:12:55 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -20,7 +20,7 @@ pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialo
 # pacman -S --noconfirm xf86-video-amdgpu
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
