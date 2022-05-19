3cd3c336a2744daa32b4128c3a8c2b346b4990c4
lzhecz <vladxpok@gmail.com>
2022-05-19 19:14:27 +0300

    Update lazy-btrfs.sh

--- lazy-btrfs.sh
+++ lazy-btrfs.sh
@@ -11,11 +11,11 @@ cd
 umount /mnt
 
 #mount subvolumes
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@ /dev/nvme0n1p2 /mnt
-mkdir /mnt/{boot,home,var}
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@home /dev/nvme0n1p2 /mnt/home
-mount -o noatime,compress=zstd,space_cache,discard=async,subvol=@var /dev/nvme0n1p2 /mnt/var
-mount /dev/nvme0n1p1 /mnt/boot
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@ /dev/nvme0n1p2 /mnt
+mkdir -p /mnt/{boot/efi,home,var}
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@home /dev/nvme0n1p2 /mnt/home
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@var /dev/nvme0n1p2 /mnt/var
+mount /dev/nvme0n1p1 /mnt/boot/efi
 
 lsblk
 
