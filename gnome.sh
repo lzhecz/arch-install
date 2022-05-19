f3ee8a182e12c8b369503eba6d42a7ea21c83020
lzhecz <vladxpok@gmail.com>
2022-05-19 22:28:12 +0300

    Update gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -12,7 +12,7 @@ git clone https://aur.archlinux.org/pikaur.git
 cd pikaur/
 makepkg -si --noconfirm
 
-#sudo pacman -S --noconfirm xorg
+sudo pacman -S --noconfirm xorg
 sudo pacman -S --noconfirm gdm gnome firefox gnome-tweaks obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 sudo pikaur -S --noconfirm telegram-desktop-bin
