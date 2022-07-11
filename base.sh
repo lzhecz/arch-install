241be768aa786dbf74ca886b5ada7ae665dc0c99
lzhecz <vladxpok@gmail.com>
2022-07-11 23:45:59 +0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ln -sf /usr/share/zoneinfo/Europe/Kiev /etc/localtime
 hwclock --systohc
 sed -i '178s/.//' /etc/locale.gen
@@ -14,25 +15,32 @@ read -sp 'Password: ' passvar
 
 echo root:$passvar | chpasswd
 
-pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
-#pacman -S cups hplip
+pacman -S dkms xorg-server xorg-xinit # nvidia-dkms # xorg
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils 
+pacman -S bash-completion openssh rsync dnsmasq openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns os-prober ntfs-3g terminus-font
+pacman -S alsa-utils wireplumber pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire libpulse # Audio
+pacman -S virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils vde2 # VM packages
+pacman -S acpi acpi_call acpid # power managment
+pacman -S tlp # power saving for notebook
+#pacman -S cups hplip # printers
 
 # pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm nvidia nvidia-dkms nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+#systemctl enable cups.service # printers
 systemctl enable NetworkManager
 systemctl enable bluetooth
-#systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
-systemctl enable firewalld
+# systemctl enable firewalld
 systemctl enable acpid
+systemctl enable tlp # power saving for notebooks
 
 useradd -m $uservar
 echo $uservar:$passvar | chpasswd
