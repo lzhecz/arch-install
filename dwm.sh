c054e0d643adc50cd02df34539de9926690d8a99
lzhecz <vladxpok@gmail.com>
2022-07-13 00:40:02 +0300

    Create dwm.sh

---
+++ dwm.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Variables
+country=Bulgaria
+
+# Options
+aur_helper=true
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+sudo reflector -c $country -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+
+# sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+# sudo firewall-cmd --add-port=1025-65535/udp --permanent
+# sudo firewall-cmd --reload
+# sudo virsh net-autostart default
+
+if [[ $aur_helper = true ]]; then
+    cd /tmp
+    git clone https://aur.archlinux.org/yay.git
+    cd yay/;makepkg -si --noconfirm;cd
+fi
+
+# Install packages
+sudo pacman -S brave polkit-gnome nitrogen lxappearance pcmanfm
+
+# Install fonts
+sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+# Pull Git repositories and install
+cd /tmp
+repos=( "dwm" "dwmblocks" "st" )
+for repo in ${repos[@]}
+do
+    git clone https://github.com/lzhecz/$repo
+    cd $repo;make;sudo make install;cd ..
+done
+
+# XSessions and dwm.desktop
+if [[ ! -d /usr/share/xsessions ]]; then
+    sudo mkdir /usr/share/xsessions
+fi
+
+cat > ./temp << "EOF"
+[Desktop Entry]
+Encoding=UTF-8
+Name=Dwm
+Comment=Dynamic window manager
+Exec=dwm
+Icon=dwm
+Type=XSession
+EOF
+sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
+
+printf "\e[1;32mDone! you can now reboot.\e[0m\n"
