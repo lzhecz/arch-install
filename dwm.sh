91ecba05de64fb5ba4a9f32078e1309bda5d3877
lzhecz <vladxpok@gmail.com>
2022-07-15 16:11:31 +0300

    Update dwm.sh

--- dwm.sh
+++ dwm.sh
@@ -22,7 +22,7 @@ if [[ $aur_helper = true ]]; then
 fi
 
 # Install packages
-sudo pacman -S brave polkit-gnome nitrogen lxappearance pcmanfm
+yay -S brave polkit-gnome nitrogen lxappearance pcmanfm
 
 # Install fonts
 sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
