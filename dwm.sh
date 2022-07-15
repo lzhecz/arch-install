0130b9ce3c24a7e558583db3ef8af7789a4d9941
lzhecz <vladxpok@gmail.com>
2022-07-15 22:53:36 +0300

    Update dwm.sh

--- dwm.sh
+++ dwm.sh
@@ -16,24 +16,25 @@ sudo reflector -c $country -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 # sudo virsh net-autostart default
 
 if [[ $aur_helper = true ]]; then
-    cd /tmp
+    cd ~
     git clone https://aur.archlinux.org/yay.git
     cd yay/;makepkg -si --noconfirm;cd
 fi
 
 # Install packages
-yay -S brave polkit-gnome nitrogen lxappearance pcmanfm
+yay -S brave polkit-gnome nitrogen lxappearance-gtk3 pcmanfm
 
 # Install fonts
 yay -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+yay -S zsh rofi picom-animations-git gpick xclip imagemagick dunst betterlockscreen-git lf flameshot tigervnc gparted gnome-disks
 
 # Pull Git repositories and install
-cd /tmp
+cd ~/.suckless
 repos=( "dwm" "dwmblocks" "st" )
 for repo in ${repos[@]}
 do
     git clone https://github.com/lzhecz/$repo
-    cd $repo;make;sudo make install;cd ..
+    cd $repo;make;sudo make install;cd
 done
 
 # XSessions and dwm.desktop
